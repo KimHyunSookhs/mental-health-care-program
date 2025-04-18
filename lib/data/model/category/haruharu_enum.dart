fa04c2a9e84e38c1f8f1a7c698608a5ab3487f9c
HyunSoo.Kim <gomtinge326@gmail.com>
2025-04-18 17:50:23 +0900

    feat : result 패턴 구현

--- lib/data/model/category/haruharu_enum.dart
+++
@@ -1,5 +0,0 @@
-enum HaruHaruEnum{
-  morning,
-  lunch,
-  night
-}
--- lib/data/model/haruharu_model.dart
+++ lib/data/model/haruharu_model.dart
@@ -1,13 +1,18 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
 
-import 'category/haruharu_enum.dart';
+import 'category/routine_category.dart';
 
 part 'haruharu_model.freezed.dart';
+part 'haruharu_model.g.dart';
 
 @freezed
 abstract class HaruHaruModel with _$HaruHaruModel {
   const factory HaruHaruModel({
-    required HaruHaruEnum category,
+    required RoutineCategory category,
     required String content,
+    required bool check,
   }) = _HaruHaruModel;
+
+  factory HaruHaruModel.fromJson(Map<String, dynamic> json) =>
+      _$HaruHaruModelFromJson(json);
 }
