c24081bcfd8233653849c2c07323a916908c397f
HyunSoo.Kim <gomtinge326@gmail.com>
2025-04-20 15:20:04 +0900

    feat: bottomNavigationBar 디자인 생성

---
+++ lib/presentation/bottom_navigation_bar/bottom_navigation_bar_scaffold.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:mental_health_care/core/routing/routes.dart';
+import 'package:mental_health_care/core/ui/color_style.dart';
+
+class BottomNavigationBarScaffold extends StatefulWidget {
+  final Widget child;
+
+  const BottomNavigationBarScaffold({super.key, required this.child});
+
+  @override
+  State<BottomNavigationBarScaffold> createState() =>
+      _BottomNavigationBarScaffoldState();
+}
+
+class _BottomNavigationBarScaffoldState
+    extends State<BottomNavigationBarScaffold> {
+  int currentIndex = 0;
+
+  void tabChange(int index) {
+    switch (index) {
+      case 0:
+        context.go(Routes.haruScreen);
+    }
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: widget.child,
+      bottomNavigationBar: BottomNavigationBar(
+          type: BottomNavigationBarType.fixed,
+          onTap: tabChange,
+          backgroundColor: Colors.white,
+          selectedItemColor: Colors.black,
+          selectedLabelStyle: TextStyle(color: Colors.black),
+          unselectedItemColor: ColorStyle.moreDarkGrey,
+          unselectedLabelStyle: TextStyle(
+            color: ColorStyle.moreDarkGrey,
+          ),
+          currentIndex: currentIndex,
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.home_outlined), label: '하루하루'),
+          ]),
+    );
+  }
+}
