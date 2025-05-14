a06e2cc09d7214f237389c94b0f64075db212b55
unknown <rlagustn070@naver.com>
2025-05-14 16:30:58 +0900

    fix : .g.dart 파일 깃허브 등록 삭제

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,7 +30,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 23
         targetSdk = 34
-        versionCode = 7
+        versionCode = 8
         versionName = "1.0.3"
     }
 
